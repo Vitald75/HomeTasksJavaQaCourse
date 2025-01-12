236fff0dff1879cb59633ad44d3003b445eba54a
Vitali_home <Vitald75@gmail.com>
2025-01-12 12:16:37 +0300

    The first task. Get factorial.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,26 @@ public class Main {
     public static void main(String[] args) {
         // main method for running home tasks
 
+        // Task 1. Get factorial
+        for (int i = -1; i<=12; i++) {
+            getFactorial(i);
+        }
 
     }
+
+    // Task 1. Get factorial
+    public static void getFactorial(int number) {
+        if (number < 0) {
+            System.out.println("Incorrect value " + number + ". It's impossible to calculate !(" + number + ")");
+        } else {
+            int result = 1;
+            for (int i = 1; i <= number; i++) {
+                result *= i;
+            }
+            System.out.println(number + "! = " + result);
+        }
+    }
+
+    //
+
 }
