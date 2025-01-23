9281c3c1fbeccd41e6d38973eade8db74d73723a
Vitali_home <Vitald75@gmail.com>
2025-01-23 22:40:29 +0300

    task12 add try catch to deal with exceptions

--- src/main/java/Task12.java
+++ src/main/java/Task12.java
@@ -3,14 +3,21 @@
 
 public class Task12 {
     public static void main(String[] args) {
-        String s = "Август, Сумма расходов равна, 2500.5 ₽, Сумма доходов равна, 555 ₽";
+        String s = "Август, Сумма расходов равна, 2500 ₽, Сумма доходов равна, 555 ₽";
         String[] array = s.split(" ");  // use " "(space) as delimeter, and get 4th and 9th elements.
 
-        int sum1 = Integer.parseInt(array[4]);
-        int sum2 = Integer.parseInt(array[9]);
 
-        System.out.println("Сумма расходов равна:" +sum1+ ".");
-        System.out.println("Сумма доходов равна:" +sum2+ ".");
+        int sum1 = 0;
+        int sum2 = 0;
+        try {
+            sum1 = Integer.parseInt(array[4]);
+            sum2 = Integer.parseInt(array[9]);
+            System.out.println("Сумма расходов равна:" +sum1+ ".");
+            System.out.println("Сумма доходов равна:" +sum2+ ".");
+
+        } catch (NumberFormatException e) {
+            System.out.println("Обнаружены нецелочисленные значения");
+        }
 
     }
 }
