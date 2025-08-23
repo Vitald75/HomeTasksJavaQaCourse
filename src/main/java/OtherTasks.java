1e63fe4bc1e94f1c13d061ed4448cbc1aa5983e9
Vitali_home <Vitald75@gmail.com>
2025-08-23 18:41:43 +0300

    OtherTasks convertArrays using Stream

--- src/main/java/OtherTasks.java
+++ src/main/java/OtherTasks.java
@@ -1,12 +1,21 @@
 import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class OtherTasks {
+
+    private int count;
+    private String name;
+
     public static void main(String[] args) {
         task1ConvertStringIntoDigits("12hghd 3hgh 678gf76f8d ");
         System.out.println(alfaTask1("123 234.7456 63 "));
 
-
         System.out.println("Результат: " + getSumOfNumbers(99999999999L));
+
+        int[] array = {12, 4, 3, 45, 3, 44, 18, 10, 10, 18 };
+        System.out.println(Set.of(convertArrayToSet(array)).toString());
+        System.out.println(Arrays.toString(convertArrayToArray(array)));
 
 
     }
@@ -42,6 +51,36 @@ public class OtherTasks {
         }
 
     }
+
+    public static Set<Integer> convertArrayToSet(int[] array) {
+
+        //Arrays.stream(array).filter(el -> el >15).toArray();
+                //collect(Collectors.toList().)
+
+        //return Arrays.stream(array).filter(el -> el >15).toArray();
+
+        return Arrays.stream(array)
+                .filter(el -> el >15)
+                .boxed()
+                .collect(Collectors.toSet());
+    }
+
+    public static int[] convertArrayToArray(int[] array) {
+
+        System.out.println(Arrays.stream(array)
+                .filter(el -> el >15).max());
+
+        Arrays.stream(array)
+                .filter(el -> el >15).map(el -> el + 40).forEach(System.out::println);
+
+
+        return Arrays.stream(array)
+                .filter(el -> el >15)
+                .distinct()
+                .toArray();
+
+    }
+
 
 
 }
